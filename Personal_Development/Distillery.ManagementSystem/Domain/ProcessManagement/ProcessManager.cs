908ec48c8573cc7e249000ed9c41948ad385bace
Jay Hevelone <127765734+Montivagant970@users.noreply.github.com>
2025-11-17 16:27:49 +0100

    Create ProcessManager.cs

---
+++ Personal_Development/Distillery.ManagementSystem/Domain/ProcessManagement/ProcessManager.cs
@@ -0,0 +1,24 @@
+using Distillery.ManagementSystem.Domain.Processes;
+using Distillery.ManagementSystem.Domain.ProcessManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distillery.ManagementSystem.Domain.ProcessManagement
+{
+    internal abstract class ProcessManager
+    {
+        /*This was thought to be the abstract class that both ProductionRunManager and a future ProcessManager
+         would inherit from, hence the more generic Process type in the returns of the methods.*/
+        protected ProcessManager() { }
+        internal abstract Process CreateNew();
+
+        internal abstract void Update();
+
+        internal abstract void AddToNotes();
+
+        internal abstract (int, Process) CloseOut();
+    }
+}
