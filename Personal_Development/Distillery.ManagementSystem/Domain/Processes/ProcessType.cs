9490068e78e68185a670730a5ba94aaf0a6c1fc8
Jay Hevelone <127765734+Montivagant970@users.noreply.github.com>
2025-11-17 16:26:00 +0100

    Create ProcessType.cs

---
+++ Personal_Development/Distillery.ManagementSystem/Domain/Processes/ProcessType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distillery.ManagementSystem.Domain.Processes
+{
+    internal enum ProcessType
+    {
+        Brewing,
+        Fermentation,
+        Distilling,
+        ProductionRun
+    }
+}
